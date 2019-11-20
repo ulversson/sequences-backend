ba858f326f37c8a74d94752feb62bd544cb20038
ulverson <marcin.walczak@gmail.com>
2019-11-20 10:58:47 +0000

    Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,28 +1,33 @@
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+lock "~> 3.11.0"
+set :application, "sequences"
+set :repo_url, "git@github.com:ulversson/sequences-backend.git"
+set :branch, "master"
+set :deploy_to, "/home/v12307mw/apps/sequences"
+set :ssh_options, keys: ["config/deploy_id_rsa"] if File.exist?("config/deploy_id_rsa")
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+#set :linked_files, %w{config/database.yml}
+set :rvm_ruby_version, "2.6.3"      # Defaults to: "default"
+if fetch(:stage) == :digital
+  set :rvm_custom_path, "/usr/share/rvm"
+end  
+#set :rvm_custom_path, "/Users/ulversson/.rvm"  # only needed if not detected# set :format, :pretty
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 namespace :deploy do
 
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:web), in: :sequence, wait: 5 do
+      execute "touch  #{release_path}/tmp/restart.txt"
+    end
+  end
+
+  before :updated, :symlink_shared do
+    on roles(:web) do
+      execute "rm #{release_path}/config/database.yml"
+      execute "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+      execute "ln -s #{shared_path}/.env #{release_path}/.env"
     end
   end
 
@@ -30,11 +35,12 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
-      #   execute :rake, 'cache:clear'
+      #   execute :rake, "cache:clear"
       # end
     end
   end
 
-  after :finishing, 'deploy:cleanup'
-
+  after :finishing, "deploy:cleanup"
+  after "deploy:publishing", "deploy:restart"
+  
 end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,9 +5,7 @@ set :stage, :production
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deployer@130.88.242.252}, roles:  %w{app db web}, port: 222
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +13,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server '130.88.242.252', user: 'deployer', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
